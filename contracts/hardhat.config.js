939054343a53a9f42a6ee96e060106d7f3e9ef5b
solomonadzape95 <adzapesolomon@gmail.com>
2025-10-23 17:08:09 +0100

    feat: refactored contracts

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    baseSepolia: {
+      url: process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
