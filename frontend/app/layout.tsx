61077086589eaed00cef1862012a61e52d480d4f
solomonadzape95 <adzapesolomon@gmail.com>
2025-10-24 13:45:39 +0100

    fix: added favixons

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,6 +10,17 @@ const myFont = localFont({
 export const metadata: Metadata = {
   title: "Keystone",
   description: "Verify once, Login anywhere",
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ]
+  },
+  manifest: '/manifest.json'
 };
 
 export default function RootLayout({
